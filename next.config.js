f66b03fb012f1d79847f46135a3c16a5b6deb69a
Richie Varghese <ric-v@outlook.com>
2025-12-11 21:34:44 +0530

    Enable unoptimized images for Azure Static Web Apps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Azure Static Web Apps will deploy as a full Next.js app with API routes
+  images: {
+    unoptimized: true, // Required for Azure Static Web Apps
+  },
 }
 
 module.exports = nextConfig
